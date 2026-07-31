2e8d01f5f4102b7dd135a30a51a55583c7e35773
a-iivanov-design <a-iivanov@mail.ru>
2026-08-01 01:08:31 +0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 window.APP_CONFIG = {
-    clubName: "ROPOGKU клуб",             // Название вашего клуба
-    logoEmoji: "🎁",                    // Эмодзи логотипа по умолчанию
-    primaryColor: "#00e676",            // Основной цвет (неоновый зеленый)
-    secondaryColor: "#ff1744",          // Дополнительный цвет (акцент/ошибки)
-    backgroundColor: "#121212",         // Цвет фона
-    cardBackground: "#212121"           // Цвет карточек
+    clubName: "CYBER CASE",
+    logoImage: "https://via.placeholder.com/150", // Сюда вставьте прямую ссылку на вашу картинку (например, .png или .jpg)
+    primaryColor: "#00e676",
+    secondaryColor: "#ff1744",
+    backgroundColor: "#121212",
+    cardBackground: "#212121"
 };
