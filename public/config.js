46957547d2c76fdc859644b9df9495cf15b98150
a-iivanov-design <a-iivanov@mail.ru>
2026-08-01 01:17:14 +0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
     clubName: "CYBER CASE",
-    logoImage: "https://via.placeholder.com/150", // Сюда вставьте прямую ссылку на вашу картинку (например, .png или .jpg)
+    logoImage: "https://via.placeholder.com/150", 
     primaryColor: "#00e676",
     secondaryColor: "#ff1744",
     backgroundColor: "#121212",
