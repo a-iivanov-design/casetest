1a306ba7c252c31ff662815bb2cf0ec9fbab1432
a-iivanov-design <a-iivanov@mail.ru>
2026-08-02 00:50:13 +0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -11,7 +11,7 @@ window.APP_CONFIG = {
     // Цветовая палитра в стиле Apple Dark Mode / Glass
     primaryColor: "#FFFFFF",     // Чистый белый акцент
     secondaryColor: "#FF453A",   // Фирменный системный красный (для ошибок/удаления)
-    backgroundColor: "#800080",  // Настоящий черный фон OLED
+    backgroundColor: "#000000",  // Настоящий черный фон OLED
     cardBackground: "rgba(255, 255, 255, 0.05)", // Полупрозрачное матовое стекло
     borderColor: "rgba(255, 255, 255, 0.1)",      // Тонкие элегантные границы
 };
