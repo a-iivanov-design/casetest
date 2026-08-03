29e16f959b9ab9bc57546d9ecbb43a718168ba7c
a-iivanov-design <a-iivanov@mail.ru>
2026-08-03 18:06:24 +0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
     // Логотип картинкой (вставь прямую ссылку на картинку сюда, чтобы вывести как на твоем скриншоте)
-    clubLogoImage: "", 
+    clubLogoImage: "/logo.phg", 
     
     // Текстовый логотип (если clubLogoImage пустая)
     clubLogoText: "CASE <span>LOUNGE</span>", 
